22d21316845d1c4b44894aaa4c4fea714c019a8f
Son Tran Thanh <286.trants@gmail.com>
2024-01-03 22:35:18 +0700

    Initial build image

---
+++ src/restore.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+set -u # `-e` omitted intentionally, but i can't remember why exactly :'(
+set -o pipefail
+
+source ./env.sh
+
+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}"
+
+if [ -z "$PASSPHRASE" ]; then
+  file_type=".dump"
+else
+  file_type=".dump.gpg"
+fi
+
+if [ $# -eq 1 ]; then
+  timestamp="$1"
+  key_suffix="${MYSQL_DATABASE}_${timestamp}${file_type}"
+else
+  echo "Finding latest backup..."
+  key_suffix=$(
+    aws $aws_args s3 ls "${s3_uri_base}/${MYSQL_DATABASE}" \
+      | sort \
+      | tail -n 1 \
+      | awk '{ print $4 }'
+  )
+fi
+
+echo "Fetching backup from S3..."
+aws $aws_args s3 cp "${s3_uri_base}/${key_suffix}" "db${file_type}"
+
+if [ -n "$PASSPHRASE" ]; then
+  echo "Decrypting backup..."
+  gpg --decrypt --batch --passphrase "$PASSPHRASE" db.dump.gpg > db.dump
+  rm db.dump.gpg
+fi
+
+conn_opts="-h $MYSQL_HOST -p $MYSQL_PORT -U $MYSQL_USER -d $MYSQL_DATABASE"
+
+echo "Restoring from backup..."
+pg_restore $conn_opts --clean --if-exists db.dump
+rm db.dump
+
+echo "Restore complete."
